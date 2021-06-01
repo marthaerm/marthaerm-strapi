76d36495a9edae962d69bbf6538fb5b3f7e8e684
Martha Rodriguez <martha.rodriguez@encora.com>
2021-06-01 18:10:15 -0500

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e6dd5e11a2937e3509a24c4269edd8ea'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '37469f93-cc1f-4679-a773-8bf379773f23'
+};
